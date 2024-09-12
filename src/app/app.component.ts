df70d88613caac74bb0a133c41ee30a2d9911c19
vpilly11 <vpilly11@gmail.com>
2024-09-12 14:19:18 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'loginProb';
+
+  formBuilder: FormBuilder = inject(FormBuilder);
+  loginForm: FormGroup = this.formBuilder.group({});
+
+  fruits = ['apples', 'bannanas', 'grapes', 'oranges'];
+
+  filter = '';
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
+
+  setFilter(event: Event) {
+    this.filter = (event.target as HTMLInputElement).value;
+    console.log(this.filter);
+  }
+
+  submit() {
+    console.log(this.loginForm?.value);
+  }
 }
